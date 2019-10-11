867654f893d647445e041e9d9477ca03b307cad1
Salocin808 <themnico@hotmail.com>
2019-10-11 14:42:10 +0200

    bugfix -> scoping the appcomponent (Singleton)

--- app/src/main/java/com/salocin/authentication/di/AppComponent.java
+++ app/src/main/java/com/salocin/authentication/di/AppComponent.java
@@ -5,11 +5,14 @@ import android.app.Application;
 
 import com.salocin.authentication.BaseApplication;
 
+import javax.inject.Singleton;
+
 import dagger.BindsInstance;
 import dagger.Component;
 import dagger.android.AndroidInjector;
 import dagger.android.support.AndroidSupportInjectionModule;
 
+@Singleton
 @Component(
         modules = {
                 AndroidSupportInjectionModule.class,
