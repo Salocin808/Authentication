f7ca4829f36325e1f6a880809b92927047577c75
Salocin808 <themnico@hotmail.com>
2019-10-11 14:35:00 +0200

    injecting logo and glide instance into auth activity

--- app/src/main/java/com/salocin/authentication/BaseApplication.java
+++ app/src/main/java/com/salocin/authentication/BaseApplication.java
@@ -1,5 +1,7 @@
 package com.salocin.authentication;
 
+import com.salocin.authentication.di.DaggerAppComponent;
+
 import dagger.android.AndroidInjector;
 import dagger.android.DaggerApplication;
 
@@ -7,6 +9,6 @@ public class BaseApplication extends DaggerApplication {
 
     @Override
     protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
-        return null;
+        return DaggerAppComponent.builder().application(this).build();
     }
 }
