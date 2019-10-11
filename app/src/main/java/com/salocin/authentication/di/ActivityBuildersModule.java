3f1a395a958b513bc8b9527b03d5064766d0c8b1
Salocin808 <themnico@hotmail.com>
2019-10-11 14:24:07 +0200

    Initial commit

---
+++ app/src/main/java/com/salocin/authentication/di/ActivityBuildersModule.java
@@ -0,0 +1,13 @@
+package com.salocin.authentication.di;
+
+import com.salocin.authentication.AuthActivity;
+
+import dagger.Module;
+import dagger.android.ContributesAndroidInjector;
+
+@Module
+public abstract class ActivityBuildersModule {
+
+    @ContributesAndroidInjector
+    abstract AuthActivity contributeAuthActivity();
+}
