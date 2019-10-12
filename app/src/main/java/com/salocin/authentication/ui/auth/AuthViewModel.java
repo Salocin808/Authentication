ecca6954a004e71bbbe55ad545ccc44fd312c972
Salocin808 <themnico@hotmail.com>
2019-10-12 17:39:12 +0200

    added user model and retrofit request to get user

--- app/src/main/java/com/salocin/authentication/ui/auth/AuthViewModel.java
+++ app/src/main/java/com/salocin/authentication/ui/auth/AuthViewModel.java
@@ -4,9 +4,15 @@ import android.util.Log;
 
 import androidx.lifecycle.ViewModel;
 
+import com.salocin.authentication.models.User;
 import com.salocin.authentication.network.auth.AuthApi;
 
 import javax.inject.Inject;
+
+import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class AuthViewModel extends ViewModel {
 
@@ -21,5 +27,30 @@ public class AuthViewModel extends ViewModel {
 
         if(authApi != null)
             Log.d(TAG, "AuthViewModel: authapi injected!!!");
+
+        authApi.getUser(1)
+                .toObservable()
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Observer<User>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(User user) {
+                        Log.d(TAG, "onNext: " + user.getEmail());
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
 }
