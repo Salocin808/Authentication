052acf1fe58463aa42f1a44362932f5e04795ef8
Salocin808 <themnico@hotmail.com>
2019-10-12 18:01:37 +0200

    converting rxJava observable to livedata

--- app/src/main/java/com/salocin/authentication/ui/auth/AuthViewModel.java
+++ app/src/main/java/com/salocin/authentication/ui/auth/AuthViewModel.java
@@ -2,6 +2,10 @@ package com.salocin.authentication.ui.auth;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.LiveDataReactiveStreams;
+import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 
 import com.salocin.authentication.models.User;
@@ -9,8 +13,6 @@ import com.salocin.authentication.network.auth.AuthApi;
 
 import javax.inject.Inject;
 
-import io.reactivex.Observer;
-import io.reactivex.Scheduler;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
@@ -20,37 +22,54 @@ public class AuthViewModel extends ViewModel {
 
     private final AuthApi authApi;
 
+    private MediatorLiveData<User> authUser = new MediatorLiveData<>();
+
     @Inject
     public AuthViewModel(AuthApi authApi) {
-
         this.authApi = authApi;
 
-        if(authApi != null)
-            Log.d(TAG, "AuthViewModel: authapi injected!!!");
+//        authApi.getUser(1)
+//                .toObservable()
+//                .subscribeOn(Schedulers.io())
+//                .subscribe(new Observer<User>() {
+//                    @Override
+//                    public void onSubscribe(Disposable d) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onNext(User user) {
+//                        Log.d(TAG, "onNext: " + user.getEmail());
+//                    }
+//
+//                    @Override
+//                    public void onError(Throwable e) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onComplete() {
+//
+//                    }
+//                });
+   }
 
-        authApi.getUser(1)
-                .toObservable()
+   public void authenticateWithId(int userId){
+        final LiveData<User> source = LiveDataReactiveStreams.fromPublisher(
+                authApi.getUser(userId)
                 .subscribeOn(Schedulers.io())
-                .subscribe(new Observer<User>() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
+        );
 
-                    }
+        authUser.addSource(source, new Observer<User>() {
+            @Override
+            public void onChanged(User user) {
+                authUser.setValue(user);
+                authUser.removeSource(source);
+            }
+        });
+   }
 
-                    @Override
-                    public void onNext(User user) {
-                        Log.d(TAG, "onNext: " + user.getEmail());
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-
-                    }
-
-                    @Override
-                    public void onComplete() {
-
-                    }
-                });
-    }
+   public LiveData<User> observeUser(){
+        return authUser;
+   }
 }
