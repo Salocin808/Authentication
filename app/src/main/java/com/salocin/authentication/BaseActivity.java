f373896ef69b152b2ecfc1b590a52b94b4481081
Salocin808 <themnico@hotmail.com>
2019-10-12 22:25:18 +0200

    added logout functionality inside main activity menu

--- app/src/main/java/com/salocin/authentication/BaseActivity.java
+++ app/src/main/java/com/salocin/authentication/BaseActivity.java
@@ -26,6 +26,7 @@ public abstract class BaseActivity extends DaggerAppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        subscribeObservers();
     }
 
     private void subscribeObservers(){
@@ -50,6 +51,7 @@ public abstract class BaseActivity extends DaggerAppCompatActivity {
                         }
 
                         case NOT_AUTHENTICATED:{
+                            navLoginScreen();
                             break;
                         }
 
--- app/src/main/java/com/salocin/authentication/ui/main/MainActivity.java
+++ app/src/main/java/com/salocin/authentication/ui/main/MainActivity.java
@@ -1,10 +1,15 @@
 package com.salocin.authentication.ui.main;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.annotation.Nullable;
+import androidx.core.view.GravityCompat;
 
 import com.salocin.authentication.BaseActivity;
+import com.salocin.authentication.R;
 
 public class MainActivity extends BaseActivity {
 
@@ -15,4 +20,35 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.logout:
+                sessionManager.logout();
+                return true;
+
+            case android.R.id.home:{
+//                if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+//                    drawerLayout.closeDrawer(GravityCompat.START);
+//                    return true;
+//                }
+//                else{
+//                    return false;
+//                }
+            }
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
