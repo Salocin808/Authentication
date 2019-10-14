bc8d9cd1873ab2705600c00bf545a2876c5ae7ac
Salocin808 <themnico@hotmail.com>
2019-10-14 17:53:39 +0200

    added custom scopes

--- app/src/main/java/com/salocin/authentication/di/ActivityBuildersModule.java
+++ app/src/main/java/com/salocin/authentication/di/ActivityBuildersModule.java
@@ -1,9 +1,11 @@
 package com.salocin.authentication.di;
 
 import com.salocin.authentication.di.auth.AuthModule;
+import com.salocin.authentication.di.auth.AuthScope;
 import com.salocin.authentication.di.auth.AuthViewModelsModule;
 import com.salocin.authentication.di.main.MainFragmentBuildersModule;
 import com.salocin.authentication.di.main.MainModule;
+import com.salocin.authentication.di.main.MainScope;
 import com.salocin.authentication.di.main.MainViewModelsModule;
 import com.salocin.authentication.ui.auth.AuthActivity;
 import com.salocin.authentication.ui.main.MainActivity;
@@ -14,6 +16,7 @@ import dagger.android.ContributesAndroidInjector;
 @Module
 public abstract class ActivityBuildersModule {
 
+    @AuthScope
     @ContributesAndroidInjector
             (
                     modules = {
@@ -23,6 +26,7 @@ public abstract class ActivityBuildersModule {
             )
     abstract AuthActivity contributeAuthActivity();
 
+    @MainScope
     @ContributesAndroidInjector
             (
                     modules = {
