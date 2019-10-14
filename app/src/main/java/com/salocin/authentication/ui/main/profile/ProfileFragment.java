d42d7ba0869bcb687df4ff6afdad55d104f41f38
Salocin808 <themnico@hotmail.com>
2019-10-14 10:46:46 +0200

    setting user info in fragment

--- app/src/main/java/com/salocin/authentication/ui/main/profile/ProfileFragment.java
+++ app/src/main/java/com/salocin/authentication/ui/main/profile/ProfileFragment.java
@@ -2,15 +2,20 @@ package com.salocin.authentication.ui.main.profile;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.salocin.authentication.R;
+import com.salocin.authentication.models.User;
+import com.salocin.authentication.ui.auth.AuthResource;
 import com.salocin.authentication.viewmodels.ViewModelProviderFactory;
 
 import javax.inject.Inject;
@@ -23,17 +28,66 @@ public class ProfileFragment extends DaggerFragment {
 
     private ProfileViewModel viewModel;
 
+    private TextView email, username, website;
+
     @Inject
     public ViewModelProviderFactory providerFactory;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
 
-        Toast.makeText(getActivity(), "Profile Fragment", Toast.LENGTH_LONG).show();
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-        viewModel = ViewModelProviders.of(this,providerFactory).get(ProfileViewModel.class);
+        email = view.findViewById(R.id.email);
+        username = view.findViewById(R.id.username);
+        website = view.findViewById(R.id.website);
 
-        return inflater.inflate(R.layout.fragment_profile, container,false);
+        viewModel = ViewModelProviders.of(this, providerFactory).get(ProfileViewModel.class);
+
+        subscribeObservers();
+    }
+
+    private void subscribeObservers() {
+        viewModel.getAuthenticatedUser().removeObservers(getViewLifecycleOwner());
+        viewModel.getAuthenticatedUser().observe(getViewLifecycleOwner(), new Observer<AuthResource<User>>() {
+            @Override
+            public void onChanged(AuthResource<User> userAuthResource) {
+                if(userAuthResource != null){
+                    switch (userAuthResource.status){
+
+                        case AUTHENTICATED: {
+                            Log.d(TAG, "onChanged: ProfileFragment: AUTHENTICATED..." +
+                                    "Authenticated as: " + userAuthResource.data.getEmail());
+                            setUserDetails(userAuthResource.data);
+                            break;
+                        }
+
+                        case ERROR:{
+                            Log.d(TAG, "onChanged: ProfileFragment: ERROR...");
+                            setErrorDetails(userAuthResource.message);
+                            break;
+                        }
+
+                    }
+                }
+            }
+        });
+    }
+
+    private void setErrorDetails(String msg) {
+        email.setText(msg);
+        username.setText("error");
+        website.setText("error");
+    }
+
+    private void setUserDetails(User user){
+        email.setText(user.getEmail());
+        username.setText(user.getUsername());
+        website.setText(user.getWebsite());
     }
 }
--- app/src/main/java/com/salocin/authentication/ui/main/profile/ProfileViewModel.java
+++ app/src/main/java/com/salocin/authentication/ui/main/profile/ProfileViewModel.java
@@ -2,18 +2,26 @@ package com.salocin.authentication.ui.main.profile;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.salocin.authentication.SessionManager;
+import com.salocin.authentication.models.User;
+import com.salocin.authentication.ui.auth.AuthResource;
 
 import javax.inject.Inject;
 
 public class ProfileViewModel extends ViewModel {
     private static final String TAG = "ProfileViewModel";
 
+    private final SessionManager sessionManager;
 
     @Inject
     public ProfileViewModel(SessionManager sessionManager) {
-        Log.d(TAG, "ProfileViewModel: injected");
+        this.sessionManager = sessionManager;
+    }
+
+    public LiveData<AuthResource<User>> getAuthenticatedUser(){
+        return sessionManager.getAuthUser();
     }
 }
