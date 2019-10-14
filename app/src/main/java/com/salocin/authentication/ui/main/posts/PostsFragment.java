e4dc74a85ce78b54f5121be5d3cf7613defa93ba
Salocin808 <themnico@hotmail.com>
2019-10-14 14:56:54 +0200

    added recyclerview to display posts

--- app/src/main/java/com/salocin/authentication/ui/main/posts/PostsFragment.java
+++ app/src/main/java/com/salocin/authentication/ui/main/posts/PostsFragment.java
@@ -10,11 +10,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.salocin.authentication.R;
 import com.salocin.authentication.models.Post;
 import com.salocin.authentication.ui.main.Resource;
+import com.salocin.authentication.util.VerticalSpacingItemDecoration;
 import com.salocin.authentication.viewmodels.ViewModelProviderFactory;
 
 import java.util.List;
@@ -28,12 +30,14 @@ public class PostsFragment extends DaggerFragment {
 
     private static final String TAG = "PostsFragment";
 
-    @Inject
-    public ViewModelProviderFactory providerFactory;
-
     private PostsViewModel viewModel;
-
     private RecyclerView recyclerView;
+
+    @Inject
+    PostsRecyclerAdapter adapter;
+
+    @Inject
+    ViewModelProviderFactory providerFactory;
 
     @Nullable
     @Override
@@ -50,6 +54,7 @@ public class PostsFragment extends DaggerFragment {
         viewModel = ViewModelProviders.of(this, providerFactory).get(PostsViewModel.class);
 
         subscribeObservers();
+        initRecyclerView();
     }
 
     private void subscribeObservers(){
@@ -60,12 +65,16 @@ public class PostsFragment extends DaggerFragment {
                 if(listResource != null){
                     switch (listResource.status){
                         case LOADING:{
+                            Log.d(TAG, "onChanged: loading...");
                             break;
                         }
                         case ERROR:{
+                            Log.e(TAG, "onChanged: error..." + listResource.message);
                             break;
                         }
                         case SUCCESS:{
+                            Log.d(TAG, "onChanged: got posts...");
+                            adapter.setPosts(listResource.data);
                             break;
                         }
                     }
@@ -74,5 +83,11 @@ public class PostsFragment extends DaggerFragment {
                 }
             }
         });
+    }
+
+    private void initRecyclerView(){
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.addItemDecoration(new VerticalSpacingItemDecoration(15));
+        recyclerView.setAdapter(adapter);
     }
 }
