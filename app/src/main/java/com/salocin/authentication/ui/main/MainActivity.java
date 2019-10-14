1e32954b9ed6a103d5b27373bd12fef03a090007
Salocin808 <themnico@hotmail.com>
2019-10-14 17:42:52 +0200

    added navigation components -> drawerlayout

--- app/src/main/java/com/salocin/authentication/ui/main/MainActivity.java
+++ app/src/main/java/com/salocin/authentication/ui/main/MainActivity.java
@@ -9,26 +9,39 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
 import com.salocin.authentication.BaseActivity;
 import com.salocin.authentication.R;
-import com.salocin.authentication.ui.main.posts.PostsFragment;
-import com.salocin.authentication.ui.main.profile.ProfileFragment;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = "MainActivity";
 
-//    private DrawerLayout drawerLayout;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        drawerLayout = findViewById(R.id.drawer_layout);
-        testFragment();
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        init();
+    }
+
+    private void init() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout);
+        NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     @Override
@@ -42,18 +55,18 @@ public class MainActivity extends BaseActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle item selection
         switch (item.getItemId()) {
+
             case R.id.logout:
                 sessionManager.logout();
                 return true;
 
-            case android.R.id.home:{
-//                if(drawerLayout.isDrawerOpen(GravityCompat.START)){
-//                    drawerLayout.closeDrawer(GravityCompat.START);
-//                    return true;
-//                }
-//                else{
-//                    return false;
-//                }
+            case android.R.id.home: {
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) { //close drawer on home click
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    return true;
+                } else {
+                    return false; //do not consume click if drawer isn't opened
+                }
             }
 
             default:
@@ -61,26 +74,43 @@ public class MainActivity extends BaseActivity {
         }
     }
 
-    private void testFragment(){
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.main_container, new PostsFragment())
-                .commit();
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_profile: {
+
+                NavOptions navOptions = new NavOptions.Builder()
+                        .setPopUpTo(R.id.main, true)
+                        .build();
+
+                Navigation.findNavController(this, R.id.nav_host_fragment).navigate(
+                        R.id.profileScreen,
+                        null,
+                        navOptions
+                );
+
+                Navigation.findNavController(this, R.id.nav_host_fragment).navigate(R.id.profileScreen);
+                break;
+            }
+            case R.id.nav_posts: {
+                if (isValidDestination(R.id.postsScreen)){
+                    Navigation.findNavController(this, R.id.nav_host_fragment).navigate(R.id.postsScreen);
+                }
+                break;
+            }
+        }
+
+        menuItem.setChecked(true);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 
+    private boolean isValidDestination(int destination){
+        return destination != Navigation.findNavController(this, R.id.nav_host_fragment).getCurrentDestination().getId();
+    }
 
-//    @Override
-//    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-//        switch (menuItem.getItemId()){
-//            case R.id.nav_profile:{
-//                break;
-//            }
-//            case R.id.nav_posts:{
-//                break;
-//            }
-//        }
-//
-//        menuItem.setChecked(true);
-//        drawerLayout.closeDrawer(GravityCompat.START);
-//        return true;
-//    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(Navigation.findNavController(this, R.id.nav_host_fragment), drawerLayout); //opens drawer with hamburger click
+    }
 }
